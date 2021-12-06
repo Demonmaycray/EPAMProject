d182cbb865482b7c6deabfe9e1ed932b9c506be7
unknown <y.korchovyi@gmail.com>
2021-12-06 11:09:27 +0200

    change file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
     backend "s3" {
-      bucket         = "my-state-aws-terraform"
-      region         = "us-east-1"
+        bucket         = "my-state-aws-terraform"
+    	key            = "terraform"
+        region         = "us-east-1"
     }
 }
 
