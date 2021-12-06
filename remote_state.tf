ed77cc27f23d4b1ae0b9d99226b6df6f59734d16
unknown <y.korchovyi@gmail.com>
2021-12-06 13:57:11 +0200

    rm s3 file

--- remote_state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-	shared_credentials_file = "/var/lib/jenkins/.aws/creds"
-        bucket         = "my-state-aws-terraform-s3-for-Epamproject"
-    	key            = "terraform"
-        region         = "us-east-1"
-    }
-}
-
