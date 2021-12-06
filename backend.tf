f4a6c09e8801bfc777f29bf9fea2c294aa83a2cb
unknown <y.korchovyi@gmail.com>
2021-12-06 11:19:53 +0200

    change file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket         = "my-state-aws-terraform"
+        bucket         = "my-state-aws-terraform-s3-for-Epamproject"
     	key            = "terraform"
         region         = "us-east-1"
     }
